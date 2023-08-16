2ec5ad7726842d0fe346c2e27410bdd2792d6cff
catmeowjiao <meowjiao321@163.com>
2023-08-16 12:46:35 +0800

    我至今没搞明白我写jrrp命令的变量名时为什么写help

--- meow_bot/plugins/jrrp/__init__.py
+++ meow_bot/plugins/jrrp/__init__.py
@@ -3,9 +3,9 @@ import random
 from nonebot import on_command
 from nonebot.adapters.onebot.v11 import Bot, MessageEvent
 
-help = on_command("jrrp", block=True, priority=2)
+jrrp = on_command("jrrp", block=True, priority=2)
 
 
-@help.handle()
+@jrrp.handle()
 async def _(bot: Bot, event: MessageEvent):
-    await help.finish(f"你今天的人品是: {random.randint(0, 100)}")
+    await jrrp.finish(f"你今天的人品是: {random.randint(0, 100)}")
