11b2d0615747a275b18181c22ab3461c45248dc5
catmeowjiao <meowjiao321@163.com>
2023-08-16 13:01:49 +0800

    修复一个用户每天会有多个人品值的问题

--- meow_bot/plugins/jrrp/__init__.py
+++ meow_bot/plugins/jrrp/__init__.py
@@ -8,4 +8,7 @@ jrrp = on_command("jrrp", block=True, priority=2)
 
 @jrrp.handle()
 async def _(bot: Bot, event: MessageEvent):
+    random.seed(
+        int(event.get_user_id()) + int((time.time() + 28800) / 86400) * 2 + 114514
+    )
     await jrrp.finish(f"你今天的人品是: {random.randint(0, 100)}")
