cc3aa5fd393ce208e9b8bd779d0575dfbb4054ff
catmeowjiao <meowjiao321@163.com>
2023-08-16 10:29:09 +0800

    修复update时没有await的问题

--- meow_bot/plugins/update/__init__.py
+++ meow_bot/plugins/update/__init__.py
@@ -10,9 +10,9 @@ update = on_command("update", permission=SUPERUSER, block=True, priority=2)
 
 @update.handle()
 async def _(bot: Bot):
-    update.send("正在更新")
+    await update.send("正在更新")
     os.system("git pull")
-    update.send("更新完成, 正在重启")
+    await update.send("更新完成, 正在重启")
     file = open("reboot.py", "w")
     file.write(str(time.time()))
     file.close()
