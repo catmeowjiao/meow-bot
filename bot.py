9e64d7ebeae91617c1f5567f04305ff84c1c3015
catmeowjiao <meowjiao321@163.com>
2023-08-16 14:30:30 +0800

    [我是傻逼] 修复超管广播禁用时找不到文件的问题

--- bot.py
+++ bot.py
@@ -13,13 +13,20 @@ nonebot.load_from_toml("pyproject.toml")
 
 if not os.path.isdir("data"):
     os.mkdir("data")
+
 if not os.path.isdir("data/chatgpt.json"):
     file = open("data/chatgpt.json", "w")
     file.write("{}")
     file.close()
+
 if not os.path.isdir("data/sign.json"):
     file = open("data/sign.json", "w")
     file.write("{}")
+    file.close()
+
+if not os.path.isdir("data/broadcast.json"):
+    file = open("data/broadcast.json", "w")
+    file.write('{"data":[]}')
     file.close()
 
 if __name__ == "__main__":
--- meow_bot/plugins/broadcast/__init__.py
+++ meow_bot/plugins/broadcast/__init__.py
@@ -8,10 +8,11 @@ broadcast = on_command("bc", permission=SUPERUSER, priority=2, block=True)
 broadcastlock = on_command("bcl", permission=SUPERUSER, priority=2, block=True)
 broadcastunlock = on_command("bcul", permission=SUPERUSER, priority=2, block=True)
 
+
 @broadcast.handle()
 async def _(bot: Bot, event: MessageEvent, arg: Message = CommandArg()):
     msg = arg.extract_plain_text().strip()
-    file = open("data/broadcast.json","r")
+    file = open("data/broadcast.json", "r")
     file_data = file.read()
     file.close()
     file_dict = json.loads(file_data)
@@ -20,32 +21,37 @@ async def _(bot: Bot, event: MessageEvent, arg: Message = CommandArg()):
     group_list = await bot.get_group_list()
     for group in group_list:
         if group["group_id"] not in file_dict["data"]:
-            await bot.send_group_msg(group_id=group["group_id"], message="[超管广播] " + msg)
+            await bot.send_group_msg(
+                group_id=group["group_id"], message="[超管广播] " + msg
+            )
     await broadcast.finish("发送成功!")
+
 
 @broadcastlock.handle()
 async def _(bot: Bot, event: MessageEvent, arg: Message = CommandArg()):
     id = int(arg.extract_plain_text().strip())
-    file = open("data/broadcast.json","r")
+    file = open("data/broadcast.json", "r")
     file_data = file.read()
     file.close()
     file_dict = json.loads(file_data)
     if id not in file_dict["data"]:
         file_dict["data"].append(id)
     file_data = json.dumps(file_dict)
-    file = open("data/broadcast.json","w")
+    file = open("data/broadcast.json", "w")
     file.write(file_data)
     await broadcastlock.finish("禁用成功!")
+
+
 @broadcastunlock.handle()
 async def _(bot: Bot, event: MessageEvent, arg: Message = CommandArg()):
     id = int(arg.extract_plain_text().strip())
-    file = open("data/broadcast.json","r")
+    file = open("data/broadcast.json", "r")
     file_data = file.read()
     file.close()
     file_dict = json.loads(file_data)
     if id in file_dict["data"]:
         file_dict["data"].remove(id)
     file_data = json.dumps(file_dict)
-    file = open("data/broadcast.json","w")
+    file = open("data/broadcast.json", "w")
     file.write(file_data)
     await broadcastunlock.finish("启用成功!")
