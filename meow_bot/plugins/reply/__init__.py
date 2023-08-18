6da8be6d27ed4cc04365dea42318cd011d233476
catmeowjiao <meowjiao321@163.com>
2023-08-18 21:27:34 +0800

    更新reply插件

--- meow_bot/plugins/reply/__init__.py
+++ meow_bot/plugins/reply/__init__.py
@@ -1,7 +1,9 @@
+import random
 from nonebot import on_message
 from nonebot.params import EventPlainText
 from nonebot.adapters.onebot.v11 import Bot, MessageEvent
 
+reply_list = ["SB","L","LLL","打字慢","亲手sudo rm -rf /*"]
 
 reply = on_message(priority=1, block=False)
 
@@ -9,4 +11,8 @@ reply = on_message(priority=1, block=False)
 @reply.handle()
 async def _(bot: Bot, event: MessageEvent):
     if event.get_user_id() == "3493487882":
-        await reply.finish("您好, SB")
+        num = random.randint(0,9)
+        if num >= 0 and num <= 2:
+            res = random.choice(reply_list)
+            await reply.finish(res)
+
