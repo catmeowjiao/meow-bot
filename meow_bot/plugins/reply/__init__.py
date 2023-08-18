a9b9e5f3770f10cb87eb5b882d5a8fca2c2aa99f
catmeowjiao <meowjiao321@163.com>
2023-08-18 21:31:59 +0800

    更新update和reply插件

--- meow_bot/plugins/reply/__init__.py
+++ meow_bot/plugins/reply/__init__.py
@@ -3,7 +3,7 @@ from nonebot import on_message
 from nonebot.params import EventPlainText
 from nonebot.adapters.onebot.v11 import Bot, MessageEvent
 
-reply_list = ["SB","L","LLL","打字慢","亲手sudo rm -rf /*"]
+reply_list = ["SB", "L", "LLL", "打字慢", "亲手sudo rm -rf /*"]
 
 reply = on_message(priority=1, block=False)
 
@@ -11,8 +11,7 @@ reply = on_message(priority=1, block=False)
 @reply.handle()
 async def _(bot: Bot, event: MessageEvent):
     if event.get_user_id() == "3493487882":
-        num = random.randint(0,9)
+        num = random.randint(0, 9)
         if num >= 0 and num <= 2:
             res = random.choice(reply_list)
             await reply.finish(res)
-
