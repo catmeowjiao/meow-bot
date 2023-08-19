fb3bd50f8a81d22edf956eb92dd48b8484b8faee
catmeowjiao <meowjiao321@163.com>
2023-08-19 22:53:10 +0800

    修复data文件每次重启重置的问题

--- bot.py
+++ bot.py
@@ -2,34 +2,34 @@ import nonebot
 import os
 from nonebot.adapters.onebot.v11 import Adapter as ONEBOT_V11Adapter
 
-
 nonebot.init()
 
 driver = nonebot.get_driver()
 driver.register_adapter(ONEBOT_V11Adapter)
 
-
 if not os.path.isdir("data"):
     os.mkdir("data")
 
-if not os.path.isdir("data/chatgpt.json"):
+if not os.path.isfile("data/chatgpt.json"):
     file = open("data/chatgpt.json", "w")
     file.write("{}")
     file.close()
 
-if not os.path.isdir("data/sign.json"):
+if not os.path.isfile("data/sign.json"):
     file = open("data/sign.json", "w")
     file.write("{}")
     file.close()
 
-if not os.path.isdir("data/broadcast.json"):
+if not os.path.isfile("data/broadcast.json"):
     file = open("data/broadcast.json", "w")
     file.write('{"data":[]}')
     file.close()
-if not os.path.isdir("data/blacklist.json"):
+
+if not os.path.isfile("data/blacklist.json"):
     file = open("data/blacklist.json", "w")
     file.write('{"data":[]}')
     file.close()
+
 nonebot.load_from_toml("pyproject.toml")
 
 if __name__ == "__main__":
