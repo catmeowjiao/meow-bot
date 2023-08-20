c3fc1f4917242ae3001791b883eea84bfae9bb56
catmeowjiao <meowjiao321@163.com>
2023-08-20 14:55:15 +0800

    添加炸群神器

---
+++ meow_bot/plugins/atall/__init__.py
@@ -0,0 +1,15 @@
+from nonebot import on_command
+from nonebot.adapters.onebot.v11 import Bot, Message, MessageEvent
+from nonebot.params import CommandArg
+from nonebot.permission import SUPERUSER
+
+atall = on_command("atall", permission=SUPERUSER, priority=2, block=True)
+
+
+@atall.handle()
+async def _(bot: Bot, event: MessageEvent):
+    user_list = await bot.get_group_member_list(group_id=event.group_id)
+    res = ""
+    for user in user_list:
+        res += f"[CQ:at,qq={user}]"
+    await bot.send_group_msg(group_id=event.group_id, message=res)
