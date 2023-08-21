21031ca1d3226a81e722cc4ff20046c7bc598297
catmeowjiao <meowjiao321@163.com>
2023-08-21 12:00:49 +0800

    update broadcast

--- meow_bot/plugins/broadcast/__init__.py
+++ meow_bot/plugins/broadcast/__init__.py
@@ -7,9 +7,13 @@ from nonebot.permission import SUPERUSER
 broadcast = on_command("bc", permission=SUPERUSER, priority=2, block=True)
 broadcastlock = on_command("bcl", permission=SUPERUSER, priority=2, block=True)
 broadcastunlock = on_command("bcul", permission=SUPERUSER, priority=2, block=True)
+broadcastlockview = on_command("bclv", permission=SUPERUSER, priority=2, block=True)
 broadcastuser = on_command("ubc", permission=SUPERUSER, priority=2, block=True)
 broadcastlockuser = on_command("ubcl", permission=SUPERUSER, priority=2, block=True)
 broadcastunlockuser = on_command("ubcul", permission=SUPERUSER, priority=2, block=True)
+broadcastlockviewuser = on_command(
+    "ubclv", permission=SUPERUSER, priority=2, block=True
+)
 
 
 @broadcast.handle()
@@ -60,6 +64,15 @@ async def _(bot: Bot, event: MessageEvent, arg: Message = CommandArg()):
     await broadcastunlock.finish("启用成功!")
 
 
+@broadcastlockview.handle()
+async def _(bot: Bot, event: MessageEvent):
+    file = open("data/broadcast.json", "r")
+    file_data = file.read()
+    file.close()
+    file_dict = json.loads(file_data)
+    await broadcast.finish(str(file_dict["data"]))
+
+
 @broadcastuser.handle()
 async def _(bot: Bot, event: MessageEvent, arg: Message = CommandArg()):
     msg = arg.extract_plain_text().strip()
@@ -104,3 +117,12 @@ async def _(bot: Bot, event: MessageEvent, arg: Message = CommandArg()):
     file = open("data/broadcastuser.json", "w")
     file.write(file_data)
     await broadcastunlock.finish("启用成功!")
+
+
+@broadcastlockviewuser.handle()
+async def _(bot: Bot, event: MessageEvent):
+    file = open("data/broadcastuser.json", "r")
+    file_data = file.read()
+    file.close()
+    file_dict = json.loads(file_data)
+    await broadcast.finish(str(file_dict["data"]))
