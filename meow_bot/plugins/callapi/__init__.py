eb05b57d05447189ed6c31242ddc353c9f7e2175
catmeowjiao <meowjiao321@163.com>
2023-08-22 08:49:36 +0800

    fix callapi

--- meow_bot/plugins/callapi/__init__.py
+++ meow_bot/plugins/callapi/__init__.py
@@ -1,14 +1,14 @@
 import nonebot
 from nonebot import on_command
 from nonebot.params import CommandArg
-from nonebot.adapters.onebot.v11 import Bot, MessageEvent
+from nonebot.adapters.onebot.v11 import Bot, Message
 from nonebot.permission import SUPERUSER
 
 callapi = on_command("callapi", permission=SUPERUSER, block=True, priority=2)
 
 
 @callapi.handle()
-async def _(bot: Bot, event: MessageEvent, msg: Message = CommandArg()):
+async def _(bot: Bot, msg: Message = CommandArg()):
     args = str(msg).split(" ")
     res = str(await bot.call_api(api=args[0], **json.loads(" ".join(args[1:]))))
     await callapi.finish(res)
