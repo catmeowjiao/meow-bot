738dc8dc2da39f5cc52671c104393452bc382a78
catmeowjiao <meowjiao321@163.com>
2023-08-27 15:43:27 +0800

    fix chatgpt

--- meow_bot/plugins/chatgpt/config.py
+++ meow_bot/plugins/chatgpt/config.py
@@ -3,9 +3,7 @@ from typing import Optional
 
 
 class Config(BaseModel, extra=Extra.ignore):
-    openai_api_key: Optional[
-        str
-    ] = "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"
+    openai_api_key: Optional[str] = ""
     openai_model_name: Optional[str] = "gpt-3.5-turbo"
     openai_max_history_limit: Optional[int] = 30
     openai_http_proxy: Optional[str] = None
