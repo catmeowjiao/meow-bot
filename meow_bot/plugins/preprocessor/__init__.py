0dc0898c548238b18a285ad6b916b8b91ac1d558
catmeowjiao <meowjiao321@163.com>
2023-09-08 21:51:02 +0800

    debug

--- meow_bot/plugins/preprocessor/__init__.py
+++ meow_bot/plugins/preprocessor/__init__.py
@@ -124,12 +124,12 @@ def change_message(event: MessageEvent, cmd_start) -> None:
 
 async def handle_api_call(_bot: Bot, api: str, data: dict[str, any]):
     print(_sudo_original_user)
-    if (
+    '''if (
         (api == "send_msg" and data["message_type"] == "private")
         or api in ["send_private_forward_msg", "send_private_msg"]
         and data["user_id"] in _sudo_original_user.keys()
     ):
-        data["user_id"] = _sudo_original_user[data["user_id"]]._sudo_original_user
+        data["user_id"] = _sudo_original_user[data["user_id"]]._sudo_original_user'''
 
 
 @get_driver().on_bot_connect
