739850adb65513c810c7ac653b2f114a7cd85f2e
catmeowjiao <meowjiao321@163.com>
2023-09-24 16:29:04 +0800

    fix help

--- meow_bot/plugins/help/__init__.py
+++ meow_bot/plugins/help/__init__.py
@@ -8,10 +8,7 @@ command_starts = list(get_driver().config.command_start)
 __plugin_meta__ = PluginMetadata(
     name="MeowBot Help Menu",
     description="MeowBot帮助菜单",
-    usage=f"""
-MeowBot Help Menu
-命令前缀: {" ".join(command_starts)}
-[命令前缀]help: 获取help指令的帮助
+    usage=f"""[命令前缀]help: 获取help指令的帮助
 [命令前缀]help list: 获取插件列表
 [命令前缀]help [插件名]: 获取该插件帮助""",
 )
@@ -37,6 +34,7 @@ async def _(event: Event, args: Message = CommandArg()):
             else:
                 await help.finish(
                     f"""MeowBot Help Menu
+命令前缀: {" ".join(command_starts)}
 插件名称: {plugin.metadata.name}
 插件描述: {plugin.metadata.description}
 使用方法:
